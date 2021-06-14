6f71924d2df643db223dc927adbe79b879e57a54
Jaeyong Kwon <kjy6380@gmail.com>
2021-06-15 00:48:44 +0900

    PC: 이미지 슬라이드 남음

--- src/components/common/Title.js
+++ src/components/common/Title.js
@@ -1,7 +1,12 @@
+import {Link} from 'react-router-dom';
+
 function Title () {
     return (
         <div className="head_title">
             <div><p>Auto 로봇</p></div>
+            <div id="start_container">
+                <Link to="/autobot"><button id="start_button" class="trade-btn2">지금 시작하기</button></Link>
+            </div>
         </div>
     )
 }
