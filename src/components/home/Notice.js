c30f0e63ce8e041e98a926d1b4fe67d0e214e7ec
Kwon JaeYong <kjy6380@gmail.com>
2021-06-25 15:21:51 +0900

    Home 공지사항 롤링 추가

--- src/components/home/Notice.js
+++ src/components/home/Notice.js
@@ -2,40 +2,64 @@ import React from "react";
 import $ from 'jquery';
 import NoticeContent from './NoticeContent';
 class Notice extends React.Component {
-    
+
+    state = {
+        notices : [
+            {id: 0, title: `당국 "국산 항체치료제 '델타 변이' 방어능력 현저히 떨어져"`, date: '06-25'},
+            {id: 1, title: '학교·아동시설서 새 집단감염…대전 교회-가족 관련 누적 72명', date: '06-25'}
+        ]
+    }
     
     componentDidMount() {
-        let height = $("#noticeTitle").height();
-        let num = $("#rolling li").length;
+        let height = $(".notice").height();
+        let num = $(".rolling li").length;
         let max = height * num;
         let move = 0;
-
+        
         const rolling = () => {
             move += height;
-            $("#rolling").animate({"top": -move}, 600, function() {
+            $(".rolling").animate({"top": -move}, 600, function() {
                 if(move >= max) {
                     $(this).css("top", 0);
                     move = 0;
                 }
             })
         }
-        setInterval(rolling, 2000);
+        
+        setInterval(rolling, 3000);
+        
+        $(".rolling").append($(".rolling li").first().clone());
     }
+    
+    // componentWillUnmount() {
+        //     clearInterval(this.rolling);
+        // }
+        
+        
+        render() {
 
+            const {notices} = this.state;
+            
+            const noticeList = notices.map(
+                ({id, title, date}) => (
+                    <NoticeContent
+                    id={id}
+                    title={title}
+                    date={date}
+                    key={id}
+                />
+            )
+        );
 
-
-    render() {
         return (
             <section className="section2">
                 <div className="notice">
                     <ul className="rolling">
-                        <NoticeContent />
-                        <NoticeContent />
+                        {noticeList}
                     </ul>
                 </div>
             </section>
         )
     }
 }
-
 export default Notice;
--- src/components/home/NoticeContent.js
+++ src/components/home/NoticeContent.js
@@ -1,20 +1,28 @@
-function NoticeContent() {
-	return (
-		<li>
-			<div>
-				<i className="far fa-circle"></i>
-			</div>
-			<div id="notice_title">
-				<p>바이낸스 대출, 차입 가능 자산에 CRV, COMP 추가, BUSD 및 USDT 프로모션 출시 </p>
-			</div>
-			<div>
-				<p>05-31</p>
-			</div>
-			{/* <div>
-				<h2>더 보기 ></h2>
-			</div> */}
-		</li>
-	)
+import React from 'react';
+
+class NoticeContent extends React.Component {
+	
+	render() {
+
+		const { id, title, date } = this.props;
+
+		return (
+			<li>
+				<div>
+					<i className="far fa-circle"></i>
+				</div>
+				<div id="notice_title">
+					<p>{title}</p>
+				</div>
+				<div>
+					<p>{date}</p>
+				</div>
+				{/* <div>
+					<h2>더 보기 ></h2>
+				</div> */}
+			</li>
+		)
+	}
 }
 
 export default NoticeContent;
