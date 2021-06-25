473c078a34f198eeb7f545d484319d1fa5a44d0f
Jaeyong Kwon <kjy6380@gmail.com>
2021-06-25 17:45:09 +0900

    navbar 작업

--- src/components/common/Menu.js
+++ src/components/common/Menu.js
@@ -4,13 +4,17 @@ function Menu() {
     return (
         <nav className="navbar">
             <div className="nav_logo">
-                <i className="fas fa-bahai"></i><Link to="/"><img src="/img/logo.png"/>&nbsp;AutoMain</Link>
+                {/*<i className="fas fa-bahai"></i>*/}<Link to="/"><img src="/img/logo.png"/>&nbsp;Penta Plant</Link>
             </div>
             <ul className="nav_menu">
-                <li><Link to="#">MTOP</Link><div> |</div></li>
-                <li><Link to="#">추천인 수익</Link><div> |</div></li>
-                <li><Link to="/autobot">AUTO 로봇</Link><div> |</div></li>
-                <li><Link to="#">특별기능</Link><div> |</div></li>
+                <li><Link to="#">MTOP</Link></li>
+                <li>|</li>
+                <li><Link to="#">추천인 수익</Link></li>
+                <li>|</li>
+                <li><Link to="/autobot">AUTO 로봇</Link></li>
+                <li>|</li>
+                <li><Link to="#">특별기능</Link></li>
+                <li>|</li>
                 <li><Link to="#">제휴</Link></li>
             </ul>
         </nav>
